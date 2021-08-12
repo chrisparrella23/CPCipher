e38ad2755db0adcf83706c236cff1d1970ecb551
Chris Parrella <61067891+chrisparrella23@users.noreply.github.com>
2021-08-11 20:11:18 -0400

    Added ComboBox to controller
    - Added ComboBox to controller.

--- CPCipher/src/controller/AppController.java
+++ CPCipher/src/controller/AppController.java
@@ -1,5 +1,19 @@
 package controller;
 
-public class AppController {
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 
+public class AppController {
+	
+	@FXML
+	private ObservableList<String> methods;
+	@FXML
+	private ComboBox<String> methodBox = new ComboBox<>();
+	
+	public AppController() {
+		methods = FXCollections.observableArrayList("Caesar", "Vigenere");
+		methodBox.setItems(methods);
+	}
 }
