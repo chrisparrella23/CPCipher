cbc57b69218f45e2bdd783e9be1e5b4dc80fa7ae
Chris Parrella <61067891+chrisparrella23@users.noreply.github.com>
2021-08-16 20:03:12 -0400

    Added method list to ComboBox.
    - Implemented observable list into combo box.

--- CPCipher/src/controller/AppController.java
+++ CPCipher/src/controller/AppController.java
@@ -1,19 +1,26 @@
 package controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 
-public class AppController {
+public class AppController implements Initializable {
 	
 	@FXML
-	private ObservableList<String> methods;
-	@FXML
-	private ComboBox<String> methodBox = new ComboBox<>();
+	private ComboBox<String> methodBox;
+	
+	ObservableList<String> methods = FXCollections.observableArrayList("Caesar", "Vigenere");
 	
 	public AppController() {
-		methods = FXCollections.observableArrayList("Caesar", "Vigenere");
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
 		methodBox.setItems(methods);
 	}
 }
